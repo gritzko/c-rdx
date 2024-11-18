dc4bddfcace7f334e748a1da43b0eda3a62b69e9
Victor Grishchenko <gritzko@pm.me>
2024-11-18 11:18:27 +0100

    Reorg the repo into separate ABC, RDX, MARK libs

---
+++ RDXJ.lex
@@ -0,0 +1,42 @@
+ws = [\r\n\t ];
+hex = [0-9a-fA-F];
+
+cp = (0x20..0xff) - ["\\];
+utf8 = cp+;
+esc = [\\] ["\\/bfnrt];
+hexEsc =  "\\u" hex{4};
+utf8esc = utf8 | esc | hexEsc;
+
+id128 = [0-9a-fA-F]+ ("-" [0-9a-fA-F]+)?;
+
+Int = [\-]? ( [0] | [1-9] [0-9]* );
+Float = ( [\-]? ( [0] | [1-9] [0-9]* )
+            ("." [0-9]+)?
+            ([eE] [\-+]? [0-9]+ )? ) - Int;
+Ref = id128 -Float -Int;
+String = ["] utf8esc* ["];
+Term = [a-zA-Z0-9_~]+ -Int -Float;
+
+Stamp = "@" id128;
+
+OpenP = "<";
+CloseP = ">";
+OpenL = "[";
+CloseL = "]";
+OpenE = "{";
+CloseE = "}";
+OpenX = "(";
+CloseX = ")";
+
+Comma = ",";
+Colon = ":";
+
+Open = (OpenP | OpenL | OpenE | OpenX) ws* (Stamp ws*)?;
+Close = (CloseP | CloseL | CloseE | CloseX) ws*;
+Inter = (Comma | Colon) ws*;
+
+delim = Open | Close | Inter;
+
+FIRST = ( Float | Int | Ref | String | Term ) (ws* Stamp)? ws*;
+
+Root = ws* ( FIRST? ( delim <: FIRST? )* ) ;
---
+++ RDXZ.h
@@ -0,0 +1,135 @@
+#ifndef ABC_RDX_Z
+#define ABC_RDX_Z
+#include "RDX.h"
+
+fun int _RDXZlit(u8 const* a, u8 const* b) {
+    b8 ap = RDXisPLEX(*a);
+    b8 bp = RDXisPLEX(*b);
+    if (ap == bp) return u8cmp(a, b);
+    return bp ? -1 : 1;
+}
+
+fun int RDXZlit(u8 const* a, u8 const* b) {
+    if (*a == *b) return 0;
+    return _RDXZlit(a, b);
+}
+
+fun int RDXZauthor($u8c const* a, $u8c const* b) {
+    u8 ta, tb;
+    $u8c keya = {}, keyb = {};
+    $u8c vala = {}, valb = {};
+    $u8c aa = $dup(*a);
+    $u8c bb = $dup(*b);
+    ok64 oa = TLVdrainkv(&ta, keya, vala, aa);
+    ok64 ob = TLVdrainkv(&tb, keyb, valb, bb);
+    u128 reva = {}, revb = {};
+    ZINTu128drain(&reva, keya);
+    ZINTu128drain(&revb, keyb);
+    int z = u64cmp(&reva._64[0], &revb._64[0]);
+    return z;
+}
+
+fun int RDXZrevision($u8c const* a, $u8c const* b) {
+    u8 ta, tb;
+    $u8c keya = {}, keyb = {};
+    $u8c vala = {}, valb = {};
+    $u8c aa = $dup(*a);
+    $u8c bb = $dup(*b);
+    ok64 oa = TLVdrainkv(&ta, keya, vala, aa);
+    ok64 ob = TLVdrainkv(&tb, keyb, valb, bb);
+    u128 reva = {}, revb = {};
+    ZINTu128drain(&reva, keya);
+    ZINTu128drain(&revb, keyb);
+    int z = u64cmp(&reva._64[1], &revb._64[1]);
+    if (z == 0) z = u64cmp(&reva._64[0], &revb._64[0]);
+    return z;
+}
+
+/**
+ 1. for values of differing types, use the type-alphabetical order
+    (`F`, `I`, `R`, `S`, `T`),
+ 2. for values of the same type:
+     1. `F` compare values numerically,
+     2. `I` also numerically,
+     3. `R` order by the value, then by the author,
+     4. `S` alphanumeric, as in `strcmp(3)`,
+     5. `T` alphanumeric.
+ **/
+fun int RDXZvalue($u8c const* a, $u8c const* b) {
+    u8 ta, tb;
+    $u8c keya = {}, keyb = {};
+    $u8c vala = {}, valb = {};
+    $u8c aa = $dup(*a);
+    $u8c bb = $dup(*b);
+    if ($empty(aa) || $empty(bb)) {
+        if (!$empty(bb)) return -1;
+        if (!$empty(aa)) return 1;
+        return 0;
+    }
+    ok64 oa = TLVdrainkv(&ta, keya, vala, aa);
+    if (ta == RDX_TUPLE) {
+        return RDXZvalue(&vala, b);
+    }
+    ok64 ob = TLVdrainkv(&tb, keyb, valb, bb);
+    while (tb == RDX_TUPLE) {
+        return RDXZvalue(a, &valb);
+    }
+    if (ta != tb) {
+        b8 fa = RDXisFIRST(ta);
+        b8 fb = RDXisFIRST(tb);
+        if (fa != fb) return fa ? -1 : 1;
+        return u8cmp(&ta, &tb);
+    }
+    switch (ta) {
+        case 'F':
+            return ZINTf64z(vala, valb);
+        case 'I':
+            return ZINTi64z(vala, valb);
+        case 'R':
+            return ZINTu128z(vala, valb);
+        case 'S':
+            return $u8cz(vala, valb);
+        case 'T':
+            return $u8cz(vala, valb);
+        case 'P':
+            return 0;  // FIXME 1st
+        case 'L':
+        case 'E':
+        case 'X':
+            return ZINTu128z(keya, keyb);
+        default:
+            return ZINTu128z(keya, keyb);
+    }
+    return 0;
+}
+
+/**
+ 1. higher revision wins (*revision-order*),
+ 2. in case of a tie, use the *value-order* (higher wins),
+ 3. in case of a tie, use the *author-order* (higher replica id wins),
+ 4. in case of a tie, we look at the same value on both sides.
+ **/
+fun int RDXZlww($u8c const* a, $u8c const* b) {
+    //
+    u8 ta, tb;
+    $u8c keya = {}, keyb = {};
+    $u8c vala = {}, valb = {};
+    $u8c aa = $dup(*a);
+    $u8c bb = $dup(*b);
+    ok64 oa = TLVdrainkv(&ta, keya, vala, aa);
+    ok64 ob = TLVdrainkv(&tb, keyb, valb, bb);
+    u128 reva = {}, revb = {};
+    ZINTu128drain(&reva, keya);
+    ZINTu128drain(&revb, keyb);
+    int z = u64cmp(&reva._64[1], &revb._64[1]);  // FIXME tuples
+    if (z == 0) {
+        z = RDXZvalue(a, b);
+        if (z == 0) {
+            z = u64cmp(&reva._64[0], &revb._64[0]);
+        }
+    }
+
+    return z;
+}
+
+#endif
